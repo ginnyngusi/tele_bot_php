3f6cfa548f8b3325f2684776bbd6f1f70f705087
bacxiunhieusuagn <106696025+ginnyngusi@users.noreply.github.com>
2022-12-22 15:49:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 const HOST = 'localhost';
-const USERNAME = 'phpmyadmin';
-const PASSWORD = 'telebot@123';
-const DATABASE = 'bot';
-$tokenBot = '5659975975:AAEIpO7Gwz3L8VHPcMT70DdEjFjkbH8gbvU'; // Put token here. We don't need to update this file anymore
+const USERNAME = '';
+const PASSWORD = '';
+const DATABASE = '';
+$tokenBot = ''; // Put token here. We don't need to update this file anymore
 
 $conn = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
 if ($conn->connect_error) {
